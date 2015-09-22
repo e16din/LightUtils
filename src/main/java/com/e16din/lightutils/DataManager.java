d0c17ac39fff0d23f265aac3abef31c4ef98200b
e16din <bigusov@gmail.com>
2015-09-22 14:12:41 +0300

    added DataManager for save to shared preferences

---
+++ src/main/java/com/e16din/lightutils/DataManager.java
@@ -0,0 +1,93 @@
+package com.e16din.lightutils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+
+public final class DataManager {
+
+    private Context context;
+    private Gson gson = new Gson();
+
+    private static class Holder {
+        public static final DataManager HOLDER_INSTANCE = new DataManager();
+    }
+
+    private DataManager() {
+    }
+
+    public static void init(final Context context) {
+        if (Holder.HOLDER_INSTANCE.context == null) {
+            Holder.HOLDER_INSTANCE.context = context.getApplicationContext();
+        }
+    }
+
+    public static DataManager getInstance() {
+        return Holder.HOLDER_INSTANCE;
+    }
+
+    /**
+     * Shared preferences
+     */
+    public SharedPreferences getDefaultSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public <T> void save(final String key, final T data) {
+        final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
+        editor.putString(key, gson.toJson(data));
+        editor.apply();
+    }
+
+    public void save(final String key, final boolean data) {
+        final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
+        editor.putBoolean(key, data);
+        editor.apply();
+    }
+
+    public void save(final String key, final String data) {
+        final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
+        editor.putString(key, data);
+        editor.apply();
+    }
+
+    public void save(final String key, final int data) {
+        final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
+        editor.putInt(key, data);
+        editor.apply();
+    }
+
+    public <T> T load(final String key, final Type type) {
+        final String string = getDefaultSharedPreferences().getString(key, null);
+        if (string == null) {
+            return null;
+        }
+        return gson.fromJson(string, type);
+    }
+
+    public String loadString(final String key) {
+        return getDefaultSharedPreferences().getString(key, null);
+    }
+
+    public boolean loadBool(final String key) {
+        return getDefaultSharedPreferences().getBoolean(key, false);
+    }
+
+    public int loadInt(final String key) {
+        return getDefaultSharedPreferences().getInt(key, -1);
+    }
+
+    public float loadFloat(final String key) {
+        return getDefaultSharedPreferences().getFloat(key, -1f);
+    }
+
+    public void clear() {
+        final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
+        editor.clear();
+        editor.apply();
+    }
+}
