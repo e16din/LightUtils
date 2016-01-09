f211c4e9fad3589cdbaeb9a082936f41593527c3
e16din <bigusov@gmail.com>
2016-01-09 15:15:10 +0300

    DataManager: add method contains, add @NonNull annotations to key parameters

--- src/main/java/com/e16din/lightutils/DataManager.java
+++ src/main/java/com/e16din/lightutils/DataManager.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 
 import com.google.gson.Gson;
 
@@ -23,7 +24,7 @@ public final class DataManager {
     private DataManager() {
     }
 
-    public static void init(final Context context) {
+    public static void init(@NonNull final Context context) {
         if (Holder.HOLDER_INSTANCE.context == null) {
             Holder.HOLDER_INSTANCE.context = context.getApplicationContext();
         }
@@ -37,50 +38,54 @@ public final class DataManager {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    public <T> void save(final String key, final T data) {
+    public boolean contains(@NonNull final String key) {
+        return getDefaultSharedPreferences().contains(key);
+    }
+
+    public <T> void save(@NonNull final String key, final T data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putString(key, gson.toJson(data));
         editor.commit();
     }
 
-    public void save(final String key, final boolean data) {
+    public void save(@NonNull final String key, final boolean data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putBoolean(key, data);
         editor.commit();
     }
 
-    public void save(final String key, final String data) {
+    public void save(@NonNull final String key, final String data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putString(key, data);
         editor.commit();
     }
 
-    public void save(final String key, final int data) {
+    public void save(@NonNull final String key, final int data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putInt(key, (int) data);
         editor.commit();
     }
 
-    public void save(final String key, final long data) {
+    public void save(@NonNull final String key, final long data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putLong(key, (long) data);
         editor.commit();
     }
 
-    public void save(final String key, final float data) {
+    public void save(@NonNull final String key, final float data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putFloat(key, (float) data);
         editor.commit();
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void save(final String key, final Set<String> data) {
+    public void save(@NonNull final String key, final Set<String> data) {
         final SharedPreferences.Editor editor = getDefaultSharedPreferences().edit();
         editor.putStringSet(key, data);
         editor.commit();
     }
 
-    public <T> T load(final String key, final Type type) {
+    public <T> T load(@NonNull final String key, final Type type) {
         final String string = getDefaultSharedPreferences().getString(key, null);
         if (string == null) {
             return null;
@@ -88,28 +93,28 @@ public final class DataManager {
         return gson.fromJson(string, type);
     }
 
-    public String loadString(final String key) {
+    public String loadString(@NonNull final String key) {
         return getDefaultSharedPreferences().getString(key, null);
     }
 
-    public boolean loadBool(final String key) {
+    public boolean loadBool(@NonNull final String key) {
         return getDefaultSharedPreferences().getBoolean(key, false);
     }
 
-    public int loadInt(final String key) {
+    public int loadInt(@NonNull final String key) {
         return getDefaultSharedPreferences().getInt(key, -1);
     }
 
-    public long loadLong(final String key) {
+    public long loadLong(@NonNull final String key) {
         return getDefaultSharedPreferences().getLong(key, -1l);
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public Set<String> loadStringSet(final String key) {
+    public Set<String> loadStringSet(@NonNull final String key) {
         return getDefaultSharedPreferences().getStringSet(key, null);
     }
 
-    public float loadFloat(final String key) {
+    public float loadFloat(@NonNull final String key) {
         return getDefaultSharedPreferences().getFloat(key, -1f);
     }
 
