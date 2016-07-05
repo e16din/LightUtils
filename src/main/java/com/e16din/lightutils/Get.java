20a25f9014c9da352ebe795a94b012277b571565
e16din <bigusov@gmail.com>
2016-07-05 14:40:30 +0300

    update Get;

--- src/main/java/com/e16din/lightutils/Get.java
+++ src/main/java/com/e16din/lightutils/Get.java
@@ -12,47 +12,49 @@ public final class Get {
     private Get() {
     }
 
-    public static int size(Object obj) {
-        return size(obj + "");
+    public static int size(String str) {
+        return str == null ? 0 : str.length();
     }
 
     public static int size(CharSequence str) {
-        return str.length();
+        return str == null ? 0 : str.length();
     }
 
     public static int size(TextView tv) {
-        return tv.length();
+        return tv == null ? 0 : tv.length();
     }
 
     public static int size(List list) {
-        return list.size();
+        return list == null ? 0 : list.size();
     }
 
     public static int size(Object[] array) {
-        return array.length;
+        return array == null ? 0 : array.length;
     }
 
+    public static String string(Object obj) {
+        if (obj instanceof List) {
+            final List list = (List) obj;
+            return "Size: " + size(list) + ", " + list;
+        } else if (obj instanceof Object[]) {
+            final Object[] array = (Object[]) obj;
+            return "Size: " + size(array) + ", " + array;
+        }
 
+        return obj + "";
+    }
+
+    public static String string(TextView tv) {
+        return tv == null ? null : tv.getText() + "";
+    }
+
+    @Deprecated
     public static int length(CharSequence str) {
         return str.length();
     }
 
+    @Deprecated
     public static int length(TextView tv) {
         return tv.length();
-    }
-
-
-    public static String string(Object obj) {
-
-        if (obj instanceof List)
-            return "size of list: " + ((List) obj).size();
-        if (obj instanceof Object[])
-            return "size of array: " + ((Object[]) obj).length;
-
-        return obj.toString();
-    }
-
-    public static String string(TextView tv) {
-        return tv.getText().toString();
     }
 }
